88afd9de3a0df025b3bde9270ba4b6d31418e4f2
= <=>
2010-08-26 00:06:54 +0200

    Closing in

--- src/main/java/org/waffle/pam/WaffleAuthModule.java
+++ src/main/java/org/waffle/pam/WaffleAuthModule.java
@@ -2,26 +2,26 @@ package org.waffle.pam;
 
 import java.io.IOException;
 import java.security.Principal;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.security.auth.Subject;
+import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
 import javax.security.auth.message.AuthException;
 import javax.security.auth.message.AuthStatus;
 import javax.security.auth.message.MessageInfo;
 import javax.security.auth.message.MessagePolicy;
-import javax.servlet.FilterConfig;
+import javax.security.auth.message.callback.CallerPrincipalCallback;
+import javax.security.auth.message.callback.GroupPrincipalCallback;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import waffle.servlet.NegotiateRequestWrapper;
 import waffle.servlet.WindowsPrincipal;
 import waffle.servlet.spi.SecurityFilterProvider;
 import waffle.servlet.spi.SecurityFilterProviderCollection;
@@ -36,7 +36,9 @@ import waffle.windows.auth.impl.WindowsAuthProviderImpl;
  * @author mikael
  */
 public class WaffleAuthModule extends AbstractHTTPServerAuthModule {
+    private static final String MODULENAME = "WaffleAuthModule";
     private static final String PRINCIPAL_SESSION_KEY = WaffleAuthModule.class.getName() + ".PRINCIPAL";
+
 
     private PrincipalFormat principalFormat = PrincipalFormat.fqn;
     private PrincipalFormat roleFormat = PrincipalFormat.fqn;
@@ -46,68 +48,84 @@ public class WaffleAuthModule extends AbstractHTTPServerAuthModule {
 
     static final Logger log = Logger.getLogger(WaffleAuthModule.class.getName());
 
+    @Override
     public void initialize(MessagePolicy requestPolicy, MessagePolicy responsePolicy, CallbackHandler handler, Map options) throws AuthException {
         super.initialize(requestPolicy, responsePolicy, handler, options);
 
-        Map<String, String> implParameters = new HashMap<String, String>();
-        if (options != null) {
-            for(Object parameterName : options.keySet()) {
-                String parameterValue = (String) options.get(parameterName);
-                log.log(Level.WARNING, "{0}={1}", new Object[]{parameterName, parameterValue});
-                if (parameterName.equals("principalFormat")) {
-                    principalFormat = PrincipalFormat.valueOf(parameterValue);
-                } else if (parameterName.equals("roleFormat")) {
-                    roleFormat = PrincipalFormat.valueOf(parameterValue);
-                } else if (parameterName.equals("allowGuestLogin")) {
-                    allowGuestLogin = Boolean.parseBoolean(parameterValue);
-                } else if (parameterName.equals("securityFilterProviders")) {
-                    providers = new SecurityFilterProviderCollection(parameterValue.split("\n"), auth);
-                } else {
-                    implParameters.put((String) parameterName,parameterValue);
+        try {
+            Map<String, String> implParameters = new HashMap<String, String>();
+            if (options != null) {
+                for(Object parameterName : options.keySet()) {
+                    if(options.get(parameterName) instanceof String) {
+                        String parameterValue = (String) options.get(parameterName);
+                        log.log(Level.WARNING, "{0}={1}", new Object[]{parameterName, parameterValue});
+                        if (parameterName.equals("principalFormat")) {
+                            principalFormat = PrincipalFormat.valueOf(parameterValue);
+                        } else if (parameterName.equals("roleFormat")) {
+                            roleFormat = PrincipalFormat.valueOf(parameterValue);
+                        } else if (parameterName.equals("allowGuestLogin")) {
+                            allowGuestLogin = Boolean.parseBoolean(parameterValue);
+                        } else if (parameterName.equals("securityFilterProviders")) {
+                            providers = new SecurityFilterProviderCollection(parameterValue.split("\n"), auth);
+                        } else {
+                            implParameters.put((String) parameterName,parameterValue);
+                        }
+                    }
                 }
             }
-        }
 
-        // create default providers if none specified
-        if (providers == null) {
-            log.severe("initializing default secuirty filter providers");
-            providers = new SecurityFilterProviderCollection(auth);
-        }
-
-        // apply provider implementation parameters
-        for (Entry<String, String> implParameter : implParameters.entrySet()) {
-            String[] classAndParameter = implParameter.getKey().split("/", 2);
-            if (classAndParameter.length == 2) {
-                try {
-                    log.log(Level.FINE, "setting {0}, {1}={2}", new Object[]{classAndParameter[0], classAndParameter[1], implParameter.getValue()});
-                    SecurityFilterProvider provider = providers.getByClassName(classAndParameter[0]);
-                    provider.initParameter(classAndParameter[1], implParameter.getValue());
-                } catch (ClassNotFoundException e) {
-                    log.log(Level.SEVERE, "invalid class: {0} in {1}", new Object[]{classAndParameter[0], implParameter.getKey()});
-                    throw new AuthException(e.getMessage());
-                } catch (Exception e) {
-                    log.log(Level.SEVERE, "{0}: error setting ''{1}'': {2}", new Object[]{classAndParameter[0], classAndParameter[1], e.getMessage()});
-                    throw new AuthException(e.getMessage());
-                }
-            } else {
-                log.log(Level.SEVERE, "Invalid parameter: {0}", implParameter.getKey());
-                throw new AuthException("Invalid parameter: " + implParameter.getKey());
+            // create default providers if none specified
+            if (providers == null) {
+                log.severe("initializing default secuirty filter providers");
+                providers = new SecurityFilterProviderCollection(auth);
             }
+
+            // apply provider implementation parameters
+            for (Entry<String, String> implParameter : implParameters.entrySet()) {
+                String[] classAndParameter = implParameter.getKey().split("/", 2);
+                if (classAndParameter.length == 2) {
+                    try {
+                        log.log(Level.FINE, "setting {0}, {1}={2}", new Object[]{classAndParameter[0], classAndParameter[1], implParameter.getValue()});
+                        SecurityFilterProvider provider = providers.getByClassName(classAndParameter[0]);
+                        provider.initParameter(classAndParameter[1], implParameter.getValue());
+                    } catch (ClassNotFoundException e) {
+                        log.log(Level.SEVERE, "invalid class: {0} in {1}", new Object[]{classAndParameter[0], implParameter.getKey()});
+                        throw new AuthException(e.getMessage());
+                    } catch (Exception e) {
+                        log.log(Level.SEVERE, "{0}: error setting ''{1}'': {2}", new Object[]{classAndParameter[0], classAndParameter[1], e.getMessage()});
+                        throw new AuthException(e.getMessage());
+                    }
+                } 
+            }
+        } catch(Exception e) {
+            log.log(Level.SEVERE,e.getMessage(),e);
         }
     }
 
 
 
     @Override
-    public AuthStatus validateRequest(MessageInfo messageInfo, Subject clientSubject, Subject serviceSubject) throws AuthException {
+    public AuthStatus validateRequest(MessageInfo messageInfo, Subject client, Subject server) throws AuthException {
         AuthStatus retVal = AuthStatus.FAILURE;
 
         HttpServletRequest request = (HttpServletRequest) messageInfo.getRequestMessage();
         HttpServletResponse response = (HttpServletResponse) messageInfo.getResponseMessage();
         HttpSession session = request.getSession(false);
 
-
         log.log(Level.INFO, "{0} {1}, contentlength: {2}", new Object[]{request.getMethod(), request.getRequestURI(), request.getContentLength()});
+
+        GroupPrincipal principal = null;
+        if (requestPolicy.isMandatory() == false || principal != null) {
+            // Unprotected page, reinstall principal if found in session
+            try {
+                setAuthenticationResult(principal, client, messageInfo);
+            } catch (Exception ex) {
+                log.log(Level.SEVERE, ex.getMessage(), ex);
+                AuthException ae = new AuthException();
+                ae.initCause(ex);
+                throw ae;
+            }
+        }
 
         if (doFilterPrincipal(request)) {
             // previously authenticated user
@@ -116,6 +134,8 @@ public class WaffleAuthModule extends AbstractHTTPServerAuthModule {
         }
 
         AuthorizationHeader authorizationHeader = new AuthorizationHeader(request);
+        System.out.println("authorizationHeader: " + request.getHeader("authorization"));
+        //log.log(Level.FINE,"AuthorizationHeader: header {0} token {1}",new Object[]{authorizationHeader.getHeader(), authorizationHeader.getToken()});
 
         // authenticate user
         if (!authorizationHeader.isNull()) {
@@ -125,12 +145,18 @@ public class WaffleAuthModule extends AbstractHTTPServerAuthModule {
             try {
                 windowsIdentity = providers.doFilter(request, response);
                 if (windowsIdentity == null) {
-                    log.log(Level.WARNING, "error getting windows user in user:");
-                    retVal = AuthStatus.FAILURE;
-                    return retVal;
+                    if(authorizationHeader.getToken() != null) {
+                        System.out.println("Tokens: " + authorizationHeader.getToken());
+                        retVal = AuthStatus.SEND_CONTINUE;
+                        return retVal;
+                    } else {
+                        log.log(Level.WARNING, "error getting windows user in user:");
+                        retVal = AuthStatus.FAILURE;
+                        return retVal;
+                    }
                 }
             } catch (Exception e) {
-                log.log(Level.WARNING, "error logging in user: {0}", e.getMessage());
+                log.log(Level.WARNING, "error loggin user: {0}", e.getMessage());
                 sendUnauthorized(response, true);
                 retVal = AuthStatus.FAILURE;
                 return retVal;
@@ -175,7 +201,7 @@ public class WaffleAuthModule extends AbstractHTTPServerAuthModule {
         }
 
         log.info("authorization required");
-        sendUnauthorized(response, false);
+        retVal = sendUnauthorized(response, false);
 
         return retVal;
     }
@@ -273,7 +299,7 @@ public class WaffleAuthModule extends AbstractHTTPServerAuthModule {
      * @param close
      *  Close connection.
      */
-    private void sendUnauthorized(HttpServletResponse response, boolean close) {
+    private AuthStatus sendUnauthorized(HttpServletResponse response, boolean close) {
         try {
             providers.sendUnauthorized(response);
             if (close) {
@@ -286,6 +312,8 @@ public class WaffleAuthModule extends AbstractHTTPServerAuthModule {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        return AuthStatus.FAILURE;
     }
 
     /**
@@ -322,5 +350,17 @@ public class WaffleAuthModule extends AbstractHTTPServerAuthModule {
      */
     public SecurityFilterProviderCollection getProviders() {
         return providers;
+    }
+
+    private void setAuthenticationResult(GroupPrincipal principal, Subject client, MessageInfo messageInfo)
+            throws IOException, UnsupportedCallbackException {
+        if(principal == null) {
+            handler.handle(new Callback[] {new CallerPrincipalCallback(client, principal)});
+            messageInfo.getMap().put(AUTH_TYPE_INFO_KEY, MODULENAME);
+        } else {
+            CallerPrincipalCallback callerPrincipalCallback = new CallerPrincipalCallback(client, principal);
+            GroupPrincipalCallback groupPrincipalCallback = new GroupPrincipalCallback(client,principal.getGroups());
+            handler.handle(new Callback[]{callerPrincipalCallback, groupPrincipalCallback});
+        }
     }
 }
